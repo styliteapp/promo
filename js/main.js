b5cc49ef5396e6d94d10b55314e5e1c67d67bda0
Grant Standridge <grant.standridge@gmail.com>
2012-09-04 14:03:56 -0400

    changed promo layout

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 (function($){
 
 $(document).ready(function(){
-	var initWinHeight = window.innerHeight,
-		winHeight;
-	$("#container").height(initWinHeight);
-	$(window).resize(function(){
-		winHeight = window.innerHeight;
-		$("#container").height(winHeight);
-	});
-	$("nav a").click(function(){
+	$("nav a").on("click", function(){
+		var articleId = $(this).attr("href");
 		$("nav a").removeClass("active");
 		$(this).addClass("active");
+		$("article").hide();
+		$(articleId).show();
 		return false;
 	});
-});
 
+	$("#launch .timer").countdown({until: new Date(2012, 8, 27, 19)});
+});
 })(jQuery);
